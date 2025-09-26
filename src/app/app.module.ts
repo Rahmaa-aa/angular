b36982364f1cf7d44c0a7fd124e200d9b2652eaf
Rahmaa-aa <rahma.ayed@esprit.tn>
2025-09-26 11:37:16 +0100

    the rest for the WS3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ListEventComponent } from './layout/list-event/list-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    ListEventComponent
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule
   ],
