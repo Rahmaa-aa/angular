d6660cf2708b8e891129394f81253ae573b886aa
Rahmaa-aa <rahma.ayed@esprit.tn>
2025-10-08 11:51:38 +0100

    last tuches for the workshop 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundError } from 'rxjs';
 import { HomeComponent } from  './layout/home/home.component';
+import { ListEventComponent } from  './layout/list-event/list-event.component';
 //import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:'home',component: HomeComponent },
   {path:'',redirectTo:'home',pathMatch:'full'},
   //{path:'**',component : NotFoundComponent}
+  {path:'/events',component: ListEventComponent  },
+  
 ];
 
 @NgModule({
--- src/app/layout/list-event/list-event.component.ts
+++ src/app/layout/list-event/list-event.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { EventService } from '../../services/event.service';
-import { Event } from '../../models/event';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-event',
   templateUrl: './list-event.component.html',
-  styleUrls: ['./list-event.component.css']
+  styleUrl: './list-event.component.css'
 })
-export class ListEventComponent implements OnInit {
-  events: Event[] = [];
+export class ListEventComponent {
 
-  constructor(private eventService: EventService) { }
-
-  ngOnInit(): void {
-    this.events = this.eventService.getEvents();
-  }
 }
